ea29277fcf601417a852d4a55d0cd42b8c3aed41
Kye Gomez <kye@swarms.world>
2025-11-07 14:18:28 -0800

    code cleanup

--- open_kimi/__init__.py
+++ open_kimi/__init__.py
@@ -8,4 +8,13 @@ from open_kimi.mla import MLA
 from open_kimi.model import KimiK2, TransformerBlock
 from open_kimi.moe import MoE
 
-__all__ = ["KimiK2", "MLA", "MoE", "TransformerBlock", "KimiDeltaAttention", "KimiLinear", "KimiLinearBlock", "chunk_kda"]
+__all__ = [
+    "KimiK2",
+    "MLA",
+    "MoE",
+    "TransformerBlock",
+    "KimiDeltaAttention",
+    "KimiLinear",
+    "KimiLinearBlock",
+    "chunk_kda",
+]
